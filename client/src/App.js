c5a77b04d46bd475b59a6bf4d4d0f87647007fef
Jean-Paul <jpzambrano.leon@gmail.com>
2023-05-18 21:26:23 -0500

    modified file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
+import { Box, Container, Toolbar } from "@mui/material";
 import Navbar from "./components/Navbar";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
+import Portfolio from "./portfolio/Portfolio";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Home />
-      <About />
     </div>
   );
 }
