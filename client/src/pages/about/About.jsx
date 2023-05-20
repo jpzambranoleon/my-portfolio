17e202ee45700246cf823c37a442b4414c267274
Jean-Paul <jpzambrano.leon@gmail.com>
2023-05-19 23:23:21 -0500

    modified About.jsx

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,43 +1,36 @@
-import { CardMedia, Container, Grid, Typography } from "@mui/material";
+import "./about.css";
 
 export default function About() {
   return (
-    <Container maxWidth="md">
-      <Grid container spacing={8}>
-        <Grid item xs={6}>
-          <CardMedia
-            component="img"
-            height="280"
-            image="/static/images/laptop_code.jpg"
-            alt="laptop_code"
-            sx={{ borderRadius: 4 }}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Typography
-            variant="body2"
-            color="primary"
-            fontWeight={500}
-            gutterBottom
-          >
-            ABOUT ME
-          </Typography>
-          <Typography variant="h6" fontWeight={500} paragraph>
-            A dedicated Front-End Web Developer based in Hot Springs, Arkansas
-            📍
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            As a junior Front-End Developer, I possess an impressive arsenal of
-            skills in HTML, CSS, JavaScript, React, CSS. I excel in designing
-            and maintaining responsive websites that offer a smooth user
-            experience. My expertise lies in crafting dynamic, engaging
-            interfaces through writing clean and optimized code and utilizing
-            cutting-edge development tools and techniques. I am also a team
-            player who thrives in collaborating with cross-functional teams to
-            produce outstanding web applications.
-          </Typography>
-        </Grid>
-      </Grid>
-    </Container>
+    <section id="about" className="about">
+      <div className="container">
+        <div className="about-content">
+          <div className="img-side">
+            <img
+              src="/static/images/laptop_code.jpg"
+              alt="mee"
+              className="img-side__main-img"
+            />
+          </div>
+          <div className="text-side">
+            <h4>About me</h4>
+            <h3>
+              A dedicated Front-end Developer <br /> based in Belgrade, Serbia
+              📍
+            </h3>
+            <p>
+              As a Junior Front-End Developer, I possess an impressive arsenal
+              of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I
+              excel in designing and maintaining responsive websites that offer
+              a smooth user experience. My expertise lies in crafting dynamic,
+              engaging interfaces through writing clean and optimized code and
+              utilizing cutting-edge development tools and techniques. I am also
+              a team player who thrives in collaborating with cross-functional
+              teams to produce outstanding web applications.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
