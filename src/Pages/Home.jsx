f7e863cf890f02445c5e3e5e73c4d9190823d297
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-28 16:59:23 -0500

    modified file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
 import About from "../components/About";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
@@ -8,6 +11,9 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
